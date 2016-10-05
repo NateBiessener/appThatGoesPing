644d513271c141672234b7c45d69744990cb2e61
Nate Biessener <nathaniel.biessener@gmail.com>
2016-10-05 16:49:18 -0500

    check time and send pings on interval

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ var port = process.env.PORT || 3140;
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/ping');
+var User = require('./models/userModel');
 
 app.listen(port, function(){
   console.log('server up on', port);
@@ -23,43 +24,80 @@ app.get('/', function(req,res){
 var userRouter = require('./routers/userRouter');
 app.use('/users', userRouter);
 
+var checkPings = function(){
+  var userQuery = User.find({}, function(err){
+    // console.log('in userQuery');
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+  //check each ping against now
+  userQuery.then(function(users){
+    // console.log('in then with', users);
+    users.forEach(function(user){
+      // console.log('in each with', user);
+      user.pings.forEach(function(ping){
+        console.log('ping ready status:', ping.fireAt < Date.now());
+        if (ping.fireAt < Date.now()) {
+          var mailOptions = {
+            from: 'nathaniel.biessener@gmail.com', // sender address
+            to: user.contactInformation.email, // list of receivers
+            subject: 'Ping!', // Subject line
+            text: ping.description //, // plaintext body
+            // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+          };
+          console.log('firing ping');
+          transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+              console.log(error);
+            }
+            else{
+              console.log('Message sent: ' + info.response);
+            };
+          });//end mailSend
+          user.pings.id(ping._id).remove();
+          user.save(function(err){
+            console.log('in save');
+            if (err) {
+              console.log(err);
+            }
+            else {
+              console.log('ping deleted');
+            }
+          })//end user.save
+        }//end ping fire
+      });
+    });
+    console.log('checked pings');
+  });
+};
+
+var minute = 60000;
+setInterval(checkPings, minute);
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    xoauth2: xoauth2.createXOAuth2Generator({
+      user: ***REMOVED***,
+      scope: 'https://mail.google.com',
+      clientId: ***REMOVED***,
+      clientSecret: ***REMOVED***,
+      refreshToken: ***REMOVED***,
+    })
+  }
+});
+
+
+
 // nodemailer test route
 // app.get('/test', function(req, res){
 //   console.log('in /test');
 //   handleSayHello(req, res);
 // });
 // function handleSayHello(req, res) {
-//   var transporter = nodemailer.createTransport({
-//     service: 'Gmail',
-//     auth: {
-//       xoauth2: xoauth2.createXOAuth2Generator({
-//         user: ***REMOVED***,
-//         scope: 'https://mail.google.com',
-//         clientId: ***REMOVED***,
-//         clientSecret: ***REMOVED***,
-//         refreshToken: ***REMOVED***,
-//       })
-//     }
-//   });
-//
-//
-//   var mailOptions = {
-//     from: 'nathaniel.biessener@gmail.com', // sender address
-//     to: 'nathaniel.biessener@gmail.com', // list of receivers
-//     subject: 'Email Example', // Subject line
-//     text: 'Hello world!' //, // plaintext body
-//     // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-//   };
-//
-//   transporter.sendMail(mailOptions, function(error, info){
-//     if(error){
-//       console.log(error);
-//       res.json({yo: 'error'});
-//     }else{
-//       console.log('Message sent: ' + info.response);
-//       res.json({yo: info.response});
-//     };
-//   });
+
 // }
 
 app.use(express.static('public'));
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -61,6 +61,40 @@ router.delete('/user', function(req, res){
 //end /user routes
 //begin /ping routes
 
+
+router.get('/ping', function(req, res){
+
+});
+
+//delete ping, expects body of {userId, pingId}
+router.delete('/ping', function(req, res){
+  var query = User.find({userId: req.body.userId}, function(err){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+  query.then(function(users){
+    //user is an array with a single index at this point, unwrapping
+    user = users[0];
+    console.log('in then with', user);
+
+    user.pings.id(req.body.pingId).remove();
+
+    user.save(function(err){
+      console.log('in save');
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+      else {
+        console.log('ping deleted');
+        res.sendStatus(200);
+      }
+    })//end user.save
+  })//end query.then
+});//end /ping post
+
 //expects body of {userID, pingDescription, pingTime, endPoints{email, sms}}
 router.post('/ping', function(req, res){
   var ping = {
