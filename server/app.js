4a670fdbfe51b8e34c98e205cec860ae0f4021f1
Nate Biessener <nathaniel.biessener@gmail.com>
2016-10-12 15:44:00 -0500

    forgot to change a thing

--- server/app.js
+++ server/app.js
@@ -167,7 +167,7 @@ app.post('/voice', (req, res) => {
 
 //15 minutes
 const INTERVAL = 60000 * 15;
-setInterval(checkPings, interval);
+setInterval(checkPings, INTERVAL);
 
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
